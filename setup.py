8b3ac7b5248c33d0a8439ab0a4c0295b34f5e4ed
Ashraful Islam <ashrafulrobin3@gmail.com>
2017-12-25 12:48:45 +0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+This is very begining of the script. I am really bored to setup everything after fresh install.
+So, I decided to write this script to setup my serveral machines. The main job of the script is
+following...
+
+1. Install dependencies and peer dependencies from libraries.json file
+2. Copy or Move some files if needed
+3. Install from source if necessary
+"""
+
+def setup(*args, **kwargs):
+	print("###########################")
+	print("##### Setup is Loading ####")
+	print("###########################")
+	
+
+
+if __name__ == '__main__':
+	setup()
